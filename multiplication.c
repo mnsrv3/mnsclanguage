500a79b53e89ff6560cd257624eabc2ffdc837cc
ravinder sharma <mnsrv3@gmail.com>
2020-01-02 22:37:54 +0530

    initial commit

---
+++ multiplication.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int m;
+    printf("hello mns\n");
+    printf("enter the number you want multiplication with\n");
+    scanf("%d",&m);
+    
+    printf("%d * 1 = %d\n",m,m*1);
+    printf("%d * 2 = %d\n",m,m*2);
+    printf("%d * 3 = %d\n",m,m*3);
+    printf("%d * 4 = %d\n",m,m*4);
+    printf("%d * 5 = %d\n",m,m*5);
+    printf("%d * 6 = %d\n",m,m*6);
+    printf("%d * 7 = %d\n",m,m*7);
+    printf("%d * 8 = %d\n",m,m*8);
+    printf("%d * 9 = %d\n",m,m*9);
+    printf("%d * 10 = %d",m,m*10);
+    
+    return 0;
+}
---
+++ specifier.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define PI 3.14
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a = 8;
+    float b = 34.433;
+    // PI = 3.24;
+    printf("hello mns\n");
+    printf("the value of a is %d and the value of b is %.2f", a, b);
+    printf("the value of PI is\t\t %f", PI);
+
+    return 0;
+}
